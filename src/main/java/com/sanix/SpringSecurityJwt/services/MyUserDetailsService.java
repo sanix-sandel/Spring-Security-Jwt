d14294665b0b66e9deb458bc7999041cad026ca1
sanix <sanicksikani@gmail.com>
2020-10-18 09:58:50 +0300

    Jwt returned

---
+++ src/main/java/com/sanix/SpringSecurityJwt/services/MyUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.sanix.SpringSecurityJwt.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayDeque;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+        return new User("foo", "foo", new ArrayDeque<>());
+    }
+}
